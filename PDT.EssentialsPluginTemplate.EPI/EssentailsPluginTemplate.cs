5281564ba4fe7b7c319db66671f94f1461f333a3
Jason T Alborough <jtalborough@gmail.com>
2019-09-04 10:04:19 -0400

    Initial seed

---
+++ PDT.EssentialsPluginTemplate.EPI/EssentailsPluginTemplate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+using Crestron.SimplSharpPro;                       				// For Basic SIMPL#Pro classes
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using PepperDash.Essentials;
+using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Core.Config;
+using PepperDash.Core;
+
+namespace EssentialsPluginTemplateEPI 
+{
+	public class EssentialsPluginTemplate : Device
+
+	{
+		public static void LoadPlugin()
+		{
+			PepperDash.Essentials.Core.DeviceFactory.AddFactoryForType("EssentialsPluginTemplate", EssentialsPluginTemplate.BuildDevice);	
+		}
+
+		public static EssentialsPluginTemplate BuildDevice(DeviceConfig dc)
+		{
+			var config = JsonConvert.DeserializeObject<EssentialsPluginTemplateConfigObject>(dc.Properties.ToString());
+			var newMe = new EssentialsPluginTemplate(dc.Key, dc.Name, config);
+			return newMe;
+		}
+
+
+		GenericSecureTcpIpClient_ForServer Client;
+
+		public EssentialsPluginTemplate(string key, string name, EssentialsPluginTemplateConfigObject config)
+			: base(key, name)
+		{
+			
+		}
+	}
+}
+
---
+++ PDT.EssentialsPluginTemplate.EPI/EssentialsPluginTemplateBridge.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro.DeviceSupport;
+
+using PepperDash.Core;
+using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Bridges;
+
+namespace EssentialsPluginTemplateEPI
+{
+	public static class EssentialsPluginTemplateBridge
+	{
+		public static void LinkToApiExt(this EssentialsPluginTemplate DspDevice, BasicTriList trilist, uint joinStart, string joinMapKey)
+		{
+			var joinMap = JoinMapHelper.GetJoinMapForDevice(joinMapKey) as EssentialsPluginTemplateBridgeJoinMap;
+
+			if (joinMap == null)
+				joinMap = new EssentialsPluginTemplateBridgeJoinMap();
+
+		}
+	}
+	public class EssentialsPluginTemplateBridgeJoinMap : JoinMapBase
+	{
+		public EssentialsPluginTemplateBridgeJoinMap()
+		{
+		}
+
+		public override void OffsetJoinNumbers(uint joinStart)
+		{
+		}
+	}
+}

05ba06661c5bfc636756f0512df8d46780a6fa8b
Andrew Welker <andrew.welker@gmail.com>
2020-08-20 10:31:33 -0600

    Update Join map and bridging methods

---
+++ PDT.EssentialsPluginTemplate.EPI/EssentialsPluginTemplateJoinMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using PepperDash.Essentials.Core;
+
+namespace PDT.EssentialsPluginTemplate.EPI
+{
+    public class EssentialsPluginTemplateBridgeJoinMap : JoinMapBaseAdvanced
+    {
+        [JoinName("IsOnline")]
+        public JoinDataComplete IsOnline = new JoinDataComplete(new JoinData {JoinNumber = 1, JoinSpan = 1},
+            new JoinMetadata
+            {
+                Description = "Device is Online",
+                JoinType = eJoinType.Digital,
+                JoinCapabilities = eJoinCapabilities.ToSIMPL
+            });
+
+
+        public EssentialsPluginTemplateBridgeJoinMap(uint joinStart):base(joinStart, typeof(EssentialsPluginTemplateBridgeJoinMap))
+        {
+            
+        }
+    }
+}

114d3af162dafa3afd51f055c69c27c76b1f63ea
Neil Dorin <ndorin@pepperdash.com>
2020-09-30 14:01:59 -0600

    Adds new classes as examples of the three primary types of device (logic, crestron and third party)

--- PDT.EssentialsPluginTemplate.EPI/EssentialsPluginTemplateDevice.cs
+++ PDT.EssentialsPluginTemplate.EPI/EssentialsPluginTemplateDevice.cs
@@ -9,7 +9,7 @@ using PepperDash.Essentials.Core.Bridges;
 namespace EssentialsPluginTemplate
 {
 	/// <summary>
-	/// Plugin device
+	/// Plugin device template for third party devices that use IBasicCommunication
 	/// </summary>
 	/// <remarks>
 	/// Rename the class to match the device plugin being developed.
@@ -17,15 +17,26 @@ namespace EssentialsPluginTemplate
 	/// <example>
 	/// "EssentialsPluginDeviceTemplate" renamed to "SamsungMdcDevice"
 	/// </example>
-	public class EssentialsPluginDeviceTemplate : EssentialsBridgeableDevice
-	{
-		// TODO [ ] Add, modify, remove properties and fields as needed for the plugin being developed
+	public class EssentialsPluginTemplateDevice : EssentialsBridgeableDevice
+    {
+        /// <summary>
+        /// It is often desirable to store the config
+        /// </summary>
+        private EssentialsPluginConfigObjectTemplate _config;
+
+        #region IBasicCommunication Properties and Constructor.  Remove if not needed.
+
+        // TODO [ ] Add, modify, remove properties and fields as needed for the plugin being developed
 		private readonly IBasicCommunication _comms;
 		private readonly GenericCommunicationMonitor _commsMonitor;
 
 		// _comms gather for ASCII based API's
 		// TODO [ ] If not using an ASCII based API, delete the properties below
 		private readonly CommunicationGather _commsGather;
+
+        /// <summary>
+        /// Set this value to that of the delimiter used by the API (if applicable)
+        /// </summary>
 		private const string CommsDelimiter = "\r";
 
 		// _comms byte buffer for HEX/byte based API's
@@ -33,7 +44,6 @@ namespace EssentialsPluginTemplate
 		private byte[] _commsByteBuffer = { };
 
 
-		private EssentialsPluginConfigObjectTemplate _config;
 
 		/// <summary>
 		/// Connects/disconnects the comms of the plugin device
@@ -75,13 +85,13 @@ namespace EssentialsPluginTemplate
 		public IntFeedback StatusFeedback { get; private set; }
 
 		/// <summary>
-		/// Plugin device constructor
+		/// Plugin device constructor for devices that need IBasicCommunication
 		/// </summary>
 		/// <param name="key"></param>
 		/// <param name="name"></param>
 		/// <param name="config"></param>
 		/// <param name="comms"></param>
-		public EssentialsPluginDeviceTemplate(string key, string name, EssentialsPluginConfigObjectTemplate config, IBasicCommunication comms)
+		public EssentialsPluginTemplateDevice(string key, string name, EssentialsPluginConfigObjectTemplate config, IBasicCommunication comms)
 			: base(key, name)
 		{
 			Debug.Console(0, this, "Constructing new {0} instance", name);
@@ -103,79 +113,28 @@ namespace EssentialsPluginTemplate
 				// device comms is IP **ELSE** device comms is RS232
 				socket.ConnectionChange += socket_ConnectionChange;
 				Connect = true;
-			}
+            }
 
-			// _comms gather for ASCII based API's
-			// TODO [ ] If not using an ASCII based API, delete the properties below
+            #region Communication data event handlers.  Comment out any that don't apply to the API type
+
+            // Only one of the below handlers should be necessary.  
+
+            // _comms gather for any API that has a defined delimiter
+			// TODO [ ] If not using an ASCII based API, remove the line below
 			_commsGather = new CommunicationGather(_comms, CommsDelimiter);
-			AddPostActivationAction(() => _commsGather.LineReceived += Handle_LineRecieved);
+			_commsGather.LineReceived += Handle_LineRecieved;
 
-			// _comms byte buffer for HEX/byte based API's
-			// TODO [ ] If not using an HEX/byte based API, delete the properties below
+			// _comms byte buffer for HEX/byte based API's with no delimiter
+            // TODO [ ] If not using an HEX/byte based API, remove the line below
 			_comms.BytesReceived += Handle_BytesReceived;
-			AddPostActivationAction(() => _comms.BytesReceived += Handle_BytesReceived);
-		}
 
-		#region Overrides of EssentialsBridgeableDevice
+            // _comms byte buffer for HEX/byte based API's with no delimiter
+            // TODO [ ] If not using an HEX/byte based API, remove the line below
+            _comms.TextReceived += Handle_TextReceived;
 
-		/// <summary>
-		/// Links the plugin device to the EISC bridge
-		/// </summary>
-		/// <param name="trilist"></param>
-		/// <param name="joinStart"></param>
-		/// <param name="joinMapKey"></param>
-		/// <param name="bridge"></param>
-		public override void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey, EiscApiAdvanced bridge)
-		{
-			var joinMap = new EssentialsPluginBridgeJoinMapTemplate(joinStart);
+            #endregion
+        }
 
-			// This adds the join map to the collection on the bridge
-			if (bridge != null)
-			{
-				bridge.AddJoinMap(Key, joinMap);
-			}
-
-			var customJoins = JoinMapHelper.TryGetJoinMapAdvancedForDevice(joinMapKey);
-
-			if (customJoins != null)
-			{
-				joinMap.SetCustomJoinData(customJoins);
-			}
-
-			Debug.Console(1, "Linking to Trilist '{0}'", trilist.ID.ToString("X"));
-			Debug.Console(0, "Linking to Bridge Type {0}", GetType().Name);
-
-			// TODO [ ] Implement bridge links as needed
-
-			// links to bridge
-			trilist.SetString(joinMap.DeviceName.JoinNumber, Name);
-
-			trilist.SetBoolSigAction(joinMap.Connect.JoinNumber, sig => Connect = sig);
-			ConnectFeedback.LinkInputSig(trilist.BooleanInput[joinMap.Connect.JoinNumber]);
-
-			StatusFeedback.LinkInputSig(trilist.UShortInput[joinMap.Status.JoinNumber]);
-			OnlineFeedback.LinkInputSig(trilist.BooleanInput[joinMap.IsOnline.JoinNumber]);
-
-			UpdateFeedbacks();
-
-			trilist.OnlineStatusChange += (o, a) =>
-			{
-				if (!a.DeviceOnLine) return;
-
-				trilist.SetString(joinMap.DeviceName.JoinNumber, Name);
-				UpdateFeedbacks();
-			};
-		}
-
-		private void UpdateFeedbacks()
-		{
-			// TODO [ ] Update as needed for the plugin being developed
-			ConnectFeedback.FireUpdate();
-			OnlineFeedback.FireUpdate();
-			StatusFeedback.FireUpdate();
-		}
-
-		#endregion
 
 		private void socket_ConnectionChange(object sender, GenericSocketStatusChageEventArgs args)
 		{
@@ -186,19 +145,27 @@ namespace EssentialsPluginTemplate
 				StatusFeedback.FireUpdate();
 		}
 
-		// TODO [ ] If not using an ASCII based API, delete the properties below
+		// TODO [ ] If not using an API with a delimeter, delete the method below
 		private void Handle_LineRecieved(object sender, GenericCommMethodReceiveTextArgs args)
 		{
 			// TODO [ ] Implement method 
 			throw new System.NotImplementedException();
 		}
 
-		// TODO [ ] If not using an HEX/byte based API, delete the properties below
+        // TODO [ ] If not using an HEX/byte based API with no delimeter,  delete the method below
 		private void Handle_BytesReceived(object sender, GenericCommMethodReceiveBytesArgs args)
 		{
 			// TODO [ ] Implement method 
 			throw new System.NotImplementedException();
 		}
+
+        // TODO [ ] If not using an ASCII based API with no delimeter, delete the method below
+        void Handle_TextReceived(object sender, GenericCommMethodReceiveTextArgs e)
+        {
+            // TODO [ ] Implement method 
+            throw new System.NotImplementedException();
+        }
+
 
 		// TODO [ ] If not using an ACII based API, delete the properties below
 		/// <summary>
@@ -239,8 +206,74 @@ namespace EssentialsPluginTemplate
 		public void Poll()
 		{
 			// TODO [ ] Update Poll method as needed for the plugin being developed
+            // Example: SendText("getstatus");
 			throw new System.NotImplementedException();
-		}
-	}
+        }
+
+        #endregion
+
+
+        #region Overrides of EssentialsBridgeableDevice
+
+        /// <summary>
+        /// Links the plugin device to the EISC bridge
+        /// </summary>
+        /// <param name="trilist"></param>
+        /// <param name="joinStart"></param>
+        /// <param name="joinMapKey"></param>
+        /// <param name="bridge"></param>
+        public override void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey, EiscApiAdvanced bridge)
+        {
+            var joinMap = new EssentialsPluginBridgeJoinMapTemplate(joinStart);
+
+            // This adds the join map to the collection on the bridge
+            if (bridge != null)
+            {
+                bridge.AddJoinMap(Key, joinMap);
+            }
+
+            var customJoins = JoinMapHelper.TryGetJoinMapAdvancedForDevice(joinMapKey);
+
+            if (customJoins != null)
+            {
+                joinMap.SetCustomJoinData(customJoins);
+            }
+
+            Debug.Console(1, "Linking to Trilist '{0}'", trilist.ID.ToString("X"));
+            Debug.Console(0, "Linking to Bridge Type {0}", GetType().Name);
+
+            // TODO [ ] Implement bridge links as needed
+
+            // links to bridge
+            trilist.SetString(joinMap.DeviceName.JoinNumber, Name);
+
+            trilist.SetBoolSigAction(joinMap.Connect.JoinNumber, sig => Connect = sig);
+            ConnectFeedback.LinkInputSig(trilist.BooleanInput[joinMap.Connect.JoinNumber]);
+
+            StatusFeedback.LinkInputSig(trilist.UShortInput[joinMap.Status.JoinNumber]);
+            OnlineFeedback.LinkInputSig(trilist.BooleanInput[joinMap.IsOnline.JoinNumber]);
+
+            UpdateFeedbacks();
+
+            trilist.OnlineStatusChange += (o, a) =>
+            {
+                if (!a.DeviceOnLine) return;
+
+                trilist.SetString(joinMap.DeviceName.JoinNumber, Name);
+                UpdateFeedbacks();
+            };
+        }
+
+        private void UpdateFeedbacks()
+        {
+            // TODO [ ] Update as needed for the plugin being developed
+            ConnectFeedback.FireUpdate();
+            OnlineFeedback.FireUpdate();
+            StatusFeedback.FireUpdate();
+        }
+
+        #endregion
+
+    }
 }
 

fcbf71078105683cc1d02d89623f63721218ed97
Trevor Payne <tpayne@pepperdash.com>
2022-07-13 12:30:15 -0500

    fix: updated AssemblyInfo copyright date

--- PDT.EssentialsPluginTemplate.EPI/Properties/AssemblyInfo.cs
+++ PDT.EssentialsPluginTemplate.EPI/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("EssentialsPluginTemplateEpi")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EssentialsPluginTemplateEpi")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyVersion("1.0.0.*")]
 
